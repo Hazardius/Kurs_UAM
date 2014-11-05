c92a221a22e7a33fd17ca26eb68e2e35df0e0ed0
Hazardius <hazardiusam@gmail.com>
2014-11-05 09:12:57 +0100

    Previous list with delete done. Pretty css.

--- Lesson 3/list/list.js
+++ Lesson 3/list/list.js
@@ -4,6 +4,14 @@
     global.UAM.elements = [];
   }
   
+  var generateFunction = function(elementName, arrayOfNames) {
+    return function () {
+        var index = arrayOfNames.indexOf(elementName);
+        arrayOfNames.splice(index, 1);
+        reorganizeUL(global.UAM.elements);
+      };
+  };
+  
   var reorganizeUL = function (arrayOfNames) {
     var UL = document.getElementById("UL");
 
@@ -14,12 +22,11 @@
     for (var i in arrayOfNames) {
       var item = document.createElement('li');
       item.appendChild(document.createTextNode(arrayOfNames[i]));
+      var elementName = item.textContent;
+      global.console.log(elementName);
       button = document.createElement("button");
-      button.onclick = function () {
-        var index = arrayOfNames.indexOf(elementName);
-        arrayOfNames.splice(index);
-        reorganizeUL(global.UAM.elements);
-      };
+      button.appendChild(document.createTextNode("Delete"));
+      button.onclick = generateFunction(elementName, arrayOfNames);
       item.appendChild(button);
       UL.appendChild(item);
     }
@@ -29,30 +36,9 @@
     if (global.UAM.elements.indexOf(elementName) == -1) {
       global.UAM.elements.push(elementName);
       reorganizeUL(global.UAM.elements);
-      /*var UL = document.getElementById("UL");
-
-      while (UL.hasChildNodes()) {
-          UL.removeChild(UL.lastChild);
-      }
-
-      for (var i in global.UAM.elements) {
-        var item = document.createElement('li');
-        item.appendChild(document.createTextNode(global.UAM.elements[i]));
-        button = document.createElement("button");
-        button.onclick = function () {
-          var index = global.UAM.elements.indexOf(elementName);
-          global.UAM.elements.splice(index);
-        };
-        item.appendChild(button);
-        UL.appendChild(item);
-      }*/
     } else {
       global.console.log("OMG! PROBLEM! VALUE ALREADY EXISTS!");
     };
-    global.console.log(global.UAM);
-  };
-  
-  global.UAM.delel = function (elementName) {
   };
 
 }(window));
